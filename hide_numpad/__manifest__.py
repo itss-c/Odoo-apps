f437dabb8d437a15b08c63568919d1f041bb2468
salehezz <saleh@itss-c.com>
2018-09-25 13:46:48 +0200

    numpad committed

---
+++ hide_numpad/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'new numpad',
+    'version': '1.0.0',
+    'category': 'Point Of Sale',
+    'author': 'mohamed ibrahim',
+    'sequence': 10,
+    'summary': 'New numpad',
+    'description': "",
+    'depends': ['point_of_sale'],
+    'data': [
+        'views/header.xml'
+    ],
+    'qweb': [
+        'static/src/xml/pos.xml',
+    ],
+    'images': ['static/description/banner.jpg'],
+    'installable': True,
+    'license': 'OPL-1'
+}
